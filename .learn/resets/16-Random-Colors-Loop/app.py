cabc04e66f92da116470bdc1f49047518585205f
arques89 <arquesjavier@gmail.com>
2022-06-13 15:17:53 +0000

    Acabado F

--- .learn/resets/16-Random-Colors-Loop/app.py
+++ .learn/resets/16-Random-Colors-Loop/app.py
@@ -18,8 +18,6 @@ def get_allStudentColors():
     example_color = get_color(1)
     students_array = []
     #your loop here
-    for i in range(1, 10):
-        numrandom = random.randint(0, 3)
-        print(get_color(numrandom))
+
 
 print(get_allStudentColors())
--- exercises/08.1-How-Much-The-Wedding-Costs/app.py
+++ exercises/08.1-How-Much-The-Wedding-Costs/app.py
@@ -2,5 +2,13 @@ user_input = int(input('How many people are coming to your wedding?\n'))
 
 if user_input <= 50:
     price = 4000
+elif user_input > 50 and user_input <= 100:
+    price = 10000
+elif user_input > 100 and user_input <= 200:
+    price = 15000
+elif user_input > 200:
+    price = 20000
+else:
+    price = 0
 
 print('Your wedding will cost '+str(price)+' dollars')
